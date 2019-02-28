80ad0b89799c1cc99818b977e9f5ccc2e19a1417
marquet <marquet@element84.com>
2019-02-28 14:25:05 -0500

    recycled code from e84-website-styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,25 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Footer from '../components/Footer'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <>
+    <Helmet>
+      <title>Space Jam Gatsby Theme</title>
+    </Helmet>
+    {/* Header/Nav component goes here */}
+    {children}
+    <Footer />
+  </>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf( PropTypes.node ),
+    PropTypes.node,
+  ]),
 }
 
 export default Layout
---
+++ src/lib/util.js
@@ -0,0 +1,92 @@
+/**
+ * isSmallDevice
+ */
+
+function isSmallDevice() {
+
+  if ( typeof window === 'undefined' ) return;
+  
+  // TODO: base this off of a configurable variable along with SCSS settings
+  
+  return window.innerWidth < 1024;
+  
+}
+  
+module.exports.isSmallDevice = isSmallDevice;
+  
+/**
+* throttle
+* Via https://remysharp.com/2010/07/21/throttling-function-calls
+*/
+  
+function throttle( fn, threshhold, scope ) {
+  
+  threshhold || ( threshhold = 250 );
+  
+  let last;
+  let deferTimer;
+  
+  return function() {
+  
+    // Check to see if we're working with an event as the first argument (standard)
+  
+    if ( arguments[0] && arguments[0].target ) {
+  
+    // Check if persist exists as a function. React handles events synthetically
+    // and nullifys them through a pooling process. Adding persist keeps this from
+    // happening until standard garbage collection so we can retain the event
+    // information through the callback
+    // More info: https://facebook.github.io/react/docs/events.html#event-pooling
+  
+      if ( typeof arguments[0].persist === 'function' ) {
+  
+        arguments[0].persist();
+  
+      }
+  
+    }
+  
+    const context = scope || this;
+    const now = +new Date();
+    const args = arguments;
+  
+    if ( last && now < last + threshhold ) {
+  
+      // hold on to it
+  
+      clearTimeout( deferTimer );
+  
+      deferTimer = setTimeout( function() {
+  
+        last = now;
+  
+        fn.apply( context, args );
+  
+      }, threshhold );
+  
+    } else {
+  
+      last = now;
+  
+      fn.apply( context, args );
+  
+    }
+  
+  };
+  
+  }
+  
+  module.exports.throttle = throttle;
+  
+  /**
+   * routeIsInternal
+   */
+  
+  function routeIsInternal( route ) {
+  
+    return route.charAt( 0 ) === '/';
+  
+}
+  
+module.exports.routeIsInternal = routeIsInternal;
+  
