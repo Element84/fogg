c501bca00bda8d0bfaa5a30bf03541d246f036d2
Colby Fayock <fayock@gmail.com>
2019-03-05 15:38:04 -0500

    theme location

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,15 +3,17 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const Logger = require('./src/lib/logger');
 
+const THEME_NAME = 'dashboard-components';
+
 exports.onCreateWebpackConfig = ({ loaders, actions }) => {
-  const logger = new Logger('space-jam-theme:onCreateWebpackConfig');
+  const logger = new Logger(`${THEME_NAME}:onCreateWebpackConfig`);
 
   let theme_location;
 
   logger.log('Adding theme to Webpack compilation');
 
   try {
-    theme_location = require.resolve('space-jam-theme');
+    theme_location = require.resolve(THEME_NAME);
   } catch (e) {
     logger.error(`Failed to load theme module: ${theme_location}; ${e}`);
     return;
@@ -34,7 +36,7 @@ exports.onCreateWebpackConfig = ({ loaders, actions }) => {
 
 // make sure src/pages exists for the filesystem source or it will error
 exports.onPreBootstrap = ({ store }) => {
-  const logger = new Logger('space-jam-theme:onPreBoostrap');
+  const logger = new Logger(`${THEME_NAME}:onPreBoostrap`);
   const { program } = store.getState();
   const dir = `${program.directory}/src/pages`;
 
@@ -47,7 +49,7 @@ exports.onPreBootstrap = ({ store }) => {
 };
 
 exports.onCreateBabelConfig = ({ actions }) => {
-  const logger = new Logger('space-jam-theme:onCreateBabelConfig');
+  const logger = new Logger(`${THEME_NAME}:onCreateBabelConfig`);
 
   logger.log('Setting babel preset: @babel/preset-react');
 
