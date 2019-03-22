2edf67e37d88bb60fc015fc593cbdc3dbdd779d7
Colby Fayock <fayock@gmail.com>
2019-03-22 16:25:35 -0400

    [JAM-248] removing console logs from button test, trying to fix create bucket script

--- build-deploy/create-bucket.sh
+++ build-deploy/create-bucket.sh
@@ -8,11 +8,11 @@ if [ "$#" -lt 1 || "$#" -lt 2 ]; then
 fi
 
 ENVIRONMENT=$1
+BUCKET=$2
 shift
 
 ARGS=$@
 
-BUCKET=$2
 BUCKET_ARN="arn:aws:s3:::$BUCKET/*"
 echo "Checking to see if ${BUCKET} exists"
 
