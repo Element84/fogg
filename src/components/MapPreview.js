f6e773c4767c60051912b370994f652c2a10e9ea
marquet <marquet@element84.com>
2019-05-10 12:19:59 -0400

    [JAM-278] changes

--- src/components/MapPreview.js
+++ src/components/MapPreview.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Map from './Map';
+import Marker from './MapMarker';
 
-import Atlas from './Atlas';
-
-const MapPreview = () => {
+const MapPreview = props => {
   // put map in smaller area with all of its stuff
   // add line at the bottom with area of interest and coordinates
-  console.log(<Atlas />);
+
+  const ALEXANDRIA = {
+    lat: 38.8048,
+    lng: -77.0469
+  };
+
+  const mapSettings = {
+    center: [ALEXANDRIA.lat, ALEXANDRIA.lng],
+    zoom: 5
+  };
+
+  const markerSettings = {
+    ...props,
+    position: [ALEXANDRIA.lat, ALEXANDRIA.lng],
+    draggable: false
+  };
+
   return (
     <>
-      <Atlas />
+      <Map {...mapSettings} />
+      <Marker {...markerSettings} />
       <h1>Area of Interest</h1>
     </>
   );
--- src/stories/components/MapPreview.stories.js
+++ src/stories/components/MapPreview.stories.js
@@ -1,83 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
 
 import MapPreview from '../../components/MapPreview';
 
 const stories = storiesOf('Components|MapPreview', module);
 
-const SidebarPanels = ({ results }) => {
-  const hasResults = Array.isArray(results) && results.length > 0;
-
-  return (
-    <>
-      {!hasResults && (
-        <>
-          <Panel header="Explore">
-            <p>Explore stuff</p>
-          </Panel>
-          <Panel header="Past Searches">
-            <ItemList
-              items={[
-                {
-                  label: 'Alexandria, VA',
-                  to: '#'
-                },
-                {
-                  label: 'Montes Claros, MG',
-                  to: '#'
-                }
-              ]}
-            />
-          </Panel>
-        </>
-      )}
-
-      {hasResults && (
-        <Panel header="Results">
-          <ItemList items={results} />
-        </Panel>
-      )}
-    </>
-  );
-};
-
-SidebarPanels.propTypes = {
-  results: PropTypes.array
+const ALEXANDRIA = {
+  lat: 38.8048,
+  lng: -77.0469
 };
 
 stories.add('Default', () => {
-  const ALEXANDRIA = {
-    lat: 38.8048,
-    lng: -77.0469
+  const mapSettings = {
+    center: [ALEXANDRIA.lat, ALEXANDRIA.lng],
+    zoom: 3
   };
 
-  function handleResolveOnSearch (coordinates) {
-    const coordinatesString = JSON.stringify(coordinates);
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve([
-          {
-            label: `#1 from ${coordinatesString}`,
-            to: '#'
-          },
-          {
-            label: `#2 from ${coordinatesString} 2`,
-            to: '#'
-          }
-        ]);
-      }, 1000);
-    });
-  }
-
   return (
-    <>
-      <MapPreview
-        defaultCenter={ALEXANDRIA}
-        zoom={3}
-        resolveOnSearch={handleResolveOnSearch}
-        SidebarComponents={SidebarPanels}
-      />
-    </>
+    <div style={{ width: '500px', height: '500px' }}>
+      <MapPreview {...mapSettings} />
+    </div>
   );
 });
