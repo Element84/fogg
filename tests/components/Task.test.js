9482b20d616b0f35b5dfabb1a09318d547ff25df
Nic Small <nic@element84.com>
2019-05-19 21:15:09 -0400

    [JAM-315] Updated tests

--- tests/components/Task.test.js
+++ tests/components/Task.test.js
@@ -7,38 +7,48 @@ describe('Task', () => {
   const data = [
     {
       id: 1,
-      name: 'Put nickles in Dwights phone',
-      windowOpen: '04/04/2019',
-      windowClose: '04/04/2019',
-      status: 'Completed'
+      properties: {
+        name: 'Put nickles in Dwights phone',
+        windowOpen: '04/04/2019',
+        windowClose: '04/04/2019',
+        status: 'Completed'
+      }
     },
     {
       id: 2,
-      name: 'Defeat Dwight in a snowball fight',
-      windowOpen: '04/04/2019',
-      windowClose: '04/04/2019',
-      status: 'Anomaly'
+      properties: {
+        name: 'Defeat Dwight in a snowball fight',
+        windowOpen: '04/04/2019',
+        windowClose: '04/04/2019',
+        status: 'Anomaly'
+      }
     },
     {
       id: 3,
-      name: 'Put more nickles in Dwights phone',
-      windowOpen: '04/04/2019',
-      windowClose: '04/05/2019',
-      status: 'Pending'
+      properties: {
+        name: 'Put more nickles in Dwights phone',
+        windowOpen: '04/04/2019',
+        windowClose: '04/05/2019',
+        status: 'Pending'
+      }
     },
     {
       id: 4,
-      name: 'Yet more nickles',
-      windowOpen: '04/04/2019',
-      windowClose: '04/08/2019',
-      status: 'Pending'
+      properties: {
+        name: 'Yet more nickles',
+        windowOpen: '04/04/2019',
+        windowClose: '04/08/2019',
+        status: 'Pending'
+      }
     },
     {
       id: 5,
-      name: 'Remove nickles so he smacks himself in the face',
-      windowOpen: '04/04/2019',
-      windowClose: '04/09/2019',
-      status: 'Partially Completed'
+      properties: {
+        name: 'Remove nickles so he smacks himself in the face',
+        windowOpen: '04/04/2019',
+        windowClose: '04/09/2019',
+        status: 'Partially Completed'
+      }
     }
   ];
 
@@ -47,7 +57,7 @@ describe('Task', () => {
   const customColumns = ['Task', 'Planned Date', 'Current Status'];
 
   describe('Render', () => {
-    const taskList = shallow(<TaskList task={data} />);
+    const taskList = shallow(<TaskList tasks={data} />);
 
     it('should render a table with default headers', () => {
       const propColumns = taskList.find('Table').prop('columns');
@@ -57,22 +67,28 @@ describe('Task', () => {
     it('should render a table with the right row values', () => {
       const propRows = taskList.find('Table').prop('rows');
       const firstRow = propRows[0];
-      expect(firstRow[0]).toEqual(data[0].name);
-      expect(firstRow[1]).toEqual(data[0].windowOpen);
-      expect(firstRow[2]).toEqual(data[0].windowClose);
-      expect(firstRow[3]).toEqual(data[0].status);
+      expect(firstRow[0]).toEqual(data[0].properties.name);
+      expect(firstRow[1]).toEqual(data[0].properties.windowOpen);
+      expect(firstRow[2]).toEqual(data[0].properties.windowClose);
+      expect(firstRow[3]).toEqual(data[0].properties.status);
     });
 
-    it('should render a button in the last cell', () => {
+    it('should render a ListItemButton in the last cell', () => {
       const propRows = taskList.find('Table').prop('rows');
       const lastColumn = propRows[0][propRows[0].length - 1];
-      const button = shallow(lastColumn);
-      expect(button.hasClass('button')).toEqual(true);
+      const listItemButton = shallow(lastColumn);
+      expect(listItemButton.find('Button')).toHaveLength(1);
+      expect(
+        listItemButton
+          .find('Button')
+          .findWhere(n => n.prop('to') === '/tasks/1')
+          .exists()
+      ).toEqual(true);
     });
   });
 
   describe('Custom Headers', () => {
-    const taskList = shallow(<TaskList headers={customColumns} task={data} />);
+    const taskList = shallow(<TaskList headers={customColumns} tasks={data} />);
 
     it('should render a table with custom headers', () => {
       const propColumns = taskList.find('Table').prop('columns');
