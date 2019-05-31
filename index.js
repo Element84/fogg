80e53c34af081b2ce6bb0c2d2eaf0e370764cb96
marquet <marquet@element84.com>
2019-05-31 11:52:56 -0400

    [JAM-323] removed invalid code

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export * from './data';
 export * from './hooks';
 export * from './models';
 export * from './ui';
-export * from './lib';
--- lib.js
+++
@@ -1,5 +0,0 @@
-export { default as formatDate } from './lib/datetime';
-export { default as regexByFieldName } from './lib/input';
-// export * from './lib/leaflet';
-// export * from './lib/logger';
-// export * from './lib/util';
