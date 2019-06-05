5647c1e76a04b5f843e15e71412255e53d5a1fe7
marquet <marquet@element84.com>
2019-06-05 13:00:42 -0400

    [JAM-324] changed sections to divs

--- src/components/OrderStatus.js
+++ src/components/OrderStatus.js
@@ -45,8 +45,8 @@ const OrderStatus = ({ order = {}, disabled = false }) => {
 
   return (
     <div className="order-status">
-      <section className="order-status-status-info-wrapper">
-        <section className="order-status-column">
+      <div className="order-status-status-info-wrapper">
+        <div className="order-status-column">
           <div className="order-status-status-indicator">
             <StatusIndicator
               activeId={orderStatus}
@@ -54,8 +54,8 @@ const OrderStatus = ({ order = {}, disabled = false }) => {
               errorList={ERROR_LIST}
             />
           </div>
-        </section>
-        <section className="order-status-column">
+        </div>
+        <div className="order-status-column">
           <p className="order-status-order-date">
             <strong>Order Date: {formatDate(orderDate)}</strong>
           </p>
@@ -63,8 +63,8 @@ const OrderStatus = ({ order = {}, disabled = false }) => {
             {/* TODO ensure this button downloads orders */}
             <Button disabled={disabled}>Download</Button>
           </div>
-        </section>
-      </section>
+        </div>
+      </div>
     </div>
   );
 };
