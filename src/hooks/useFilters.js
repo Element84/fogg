f390dc11282b0020f49b36ad7fddcb15c8bede2b
Colby Fayock <fayock@gmail.com>
2019-06-13 13:58:52 -0300

    [JAM-409] updating logic to use reusable function. adding comments

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -8,6 +8,11 @@ export default function useFilters (availableFilters) {
     available: availableFilters || []
   });
 
+  /**
+   * openFilters
+   * @description Triggers a UI update to open the Filters pane
+   */
+
   function openFilters () {
     const updatedFilterState = {
       ...filters,
@@ -17,59 +22,38 @@ export default function useFilters (availableFilters) {
     return updatedFilterState;
   }
 
+  /**
+   * storeFilterChanges
+   * @description Stores any temporary unsaved changes for the filter state
+   */
+
   function storeFilterChanges (changes = []) {
-    const filterChanges = [...filters.unsaved];
-
-    changes.forEach(filter => {
-      const oldFilter = filterChanges.find(
-        filterChange => filterChange.id === filter.id
-      );
-      if (!oldFilter) {
-        filterChanges.push(filter);
-      }
-    });
-
     const updatedFilterState = {
       ...filters,
-      unsaved: filterChanges.map(filterChange => {
-        const newFilterChange =
-          changes.find(filter => filter.id === filterChange.id) || {};
-        return {
-          ...filterChange,
-          ...newFilterChange
-        };
-      })
+      unsaved: concatFilters(filters.unsaved, changes)
     };
     updateFilters(updatedFilterState);
     return updatedFilterState;
   }
+
+  /**
+   * setActiveFilters
+   * @description Applies any new changes to the active filters
+   */
 
   function setActiveFilters (changes = []) {
-    const filterChanges = [...filters.active];
-
-    changes.forEach(filter => {
-      const oldFilter = filterChanges.find(
-        filterChange => filterChange.id === filter.id
-      );
-      if (!oldFilter) {
-        filterChanges.push(filter);
-      }
-    });
-
     const updatedFilterState = {
       ...filters,
-      active: filterChanges.map(filterChange => {
-        const newFilterChange =
-          changes.find(filter => filter.id === filterChange.id) || {};
-        return {
-          ...filterChange,
-          ...newFilterChange
-        };
-      })
+      active: concatFilters(filters.active, changes)
     };
     updateFilters(updatedFilterState);
     return updatedFilterState;
   }
+
+  /**
+   * saveFilterChanges
+   * @description Stores any new unsaved changes to active and clears unsaved
+   */
 
   function saveFilterChanges () {
     const updatedFilterState = {
@@ -84,6 +68,11 @@ export default function useFilters (availableFilters) {
     return updatedFilterState;
   }
 
+  /**
+   * cancelFilterChanges
+   * @description Clears out any unsaved changes
+   */
+
   function cancelFilterChanges () {
     const updatedFilterState = {
       ...filters,
@@ -93,6 +82,11 @@ export default function useFilters (availableFilters) {
     updateFilters(updatedFilterState);
     return updatedFilterState;
   }
+
+  /**
+   * clearActiveFilters
+   * @description Clears out all active and unsaved filters
+   */
 
   function clearActiveFilters () {
     const updatedFilterState = {
@@ -104,6 +98,12 @@ export default function useFilters (availableFilters) {
     updateFilters(updatedFilterState);
     return updatedFilterState;
   }
+
+  /**
+   * buildAvailableFilters
+   * @description Sets up a filters list of all available filters with their active state.
+   *     If the UI is open, we also show the unsaved state to allow users to edit
+   */
 
   function buildAvailableFilters () {
     let filtersSet = concatFilters(
@@ -136,13 +136,18 @@ export default function useFilters (availableFilters) {
 /**
  * concatFilters
  * @description Concats all arrays that are passed in and their values
+ *     Each ID uses the last available value in the argument arrays
  */
 
 function concatFilters () {
   const currentArguments = Array.prototype.slice.call(arguments);
   const allFilters = [];
 
+  // Loop through all args, allowing someone to pass in as many arrays
+  // of filters as they wish
+
   currentArguments.forEach(filterSet => {
+    // Loop through each set of filters and overlay it into our main array
     filterSet.forEach(filter => {
       const existingIndex = allFilters.findIndex(
         existingFilter => existingFilter.id === filter.id
