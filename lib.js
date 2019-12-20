1d769f35ad7261aa6c166f617f295907e5d019a9
Colby Fayock <fayock@gmail.com>
2019-12-20 17:30:00 -0500

    [JAM-896] reomving search and leaflet from lib exporst to try to avoid lefalet issues

--- lib.js
+++ lib.js
@@ -2,12 +2,10 @@ import * as datetime from './src/lib/datetime';
 import * as device from './src/lib/device';
 import * as filters from './src/lib/filters';
 import * as input from './src/lib/input';
-import * as leaflet from './src/lib/leaflet';
 import * as location from './src/lib/location';
 import * as logger from './src/lib/logger';
 import * as request from './src/lib/request';
 import * as routes from './src/lib/routes';
-import * as search from './src/lib/search';
 import * as util from './src/lib/util';
 
 export {
@@ -15,11 +13,9 @@ export {
   device,
   filters,
   input,
-  leaflet,
   location,
   logger,
   request,
   routes,
-  search,
   util
 };
