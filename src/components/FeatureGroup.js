edc0d423b0a49608621387a84249625405e8da65
Colby Fayock <fayock@gmail.com>
2020-02-06 11:49:46 -0500

    [JAM-896] removing react-leaflet from null loader

--- src/components/FeatureGroup.js
+++ src/components/FeatureGroup.js
@@ -4,10 +4,6 @@ import { MapLayer, withLeaflet } from 'react-leaflet';
 // Recreates react-leaflet's FeatureGroup with the ability to pass in custom FeatureGroup
 // via https://github.com/PaulLeCam/react-leaflet/blob/master/src/FeatureGroup.js
 
-if (!MapLayer) {
-  MapLayer = {};
-}
-
 class FeatureGroup extends MapLayer {
   createLeafletElement (props) {
     let el = this.props.featureGroup;
