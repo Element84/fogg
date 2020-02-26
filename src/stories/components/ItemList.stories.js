6e6b9f64b4d638a5427d78a6ed0478cbb7751e42
Justin Kuo <jkuo@element84.com>
2020-02-26 15:54:59 +0000

    Merge branch 'JAM-998-multiple-products-search-results' of repo.element84.com:fogg/fogg into JAM-998-multiple-products-search-results

--- src/stories/components/ItemList.stories.js
+++ src/stories/components/ItemList.stories.js
@@ -135,3 +135,48 @@ stories.add('Mouse Events', () => {
     </>
   );
 });
+
+stories.add('Checkbox', () => {
+  const items = [
+    {
+      id: 'item-1',
+      thumb: 'https://www.placecage.com/200/200',
+      label: 'Why',
+      to: '#'
+    },
+    {
+      id: 'item-2',
+      thumb: 'https://www.placecage.com/300/300',
+      label: 'Not (Checked by default)',
+      to: '#',
+      isChecked: true
+    },
+    {
+      id: 'item-3',
+      thumb: 'https://www.placecage.com/400/400',
+      label: 'Nic',
+      to: '#'
+    },
+    {
+      id: 'item-4',
+      thumb: 'https://www.placecage.com/500/500',
+      label: 'Cage (No Icon, No To)',
+      icon: false
+    },
+    {
+      id: 'item-5',
+      thumb: 'https://www.placecage.com/600/600',
+      label: '?  (No To)'
+    }
+  ];
+
+  function onCheck ({ target = {} } = {}, event) {
+    action(target.value)(target.checked);
+  }
+
+  return (
+    <>
+      <ItemList items={items} onCheck={onCheck} />
+    </>
+  );
+});
