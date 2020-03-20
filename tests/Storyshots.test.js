2d4510e2b87995fb01948c84167a3cf394dd42d7
Colby Fayock <fayock@gmail.com>
2020-03-20 07:59:51 -0400

    [JAM-1072] fixing snapshots

--- tests/Storyshots.test.js
+++ tests/Storyshots.test.js
@@ -1,8 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
 // TODO: Figure out how to include excluded components
-// TODO: InputRange - Move to https://www.npmjs.com/package/rc-slider?
+// TODO: Lens, Map - Leaflet, etc
+// TODO: InputRange - Mounting issue? Move to https://www.npmjs.com/package/rc-slider?
+// TODO: Modal - Can't find the mounting location when ran
+// TODO: Search Date, Search Box, Form Input, Datetime - Changes "today" daily, meaning component changes
 
 initStoryshots({
-  storyKindRegex: /^((?!.*?(Lens|Map|Search Complete|Input Range|Modal)).)*$/
+  storyKindRegex: /^((?!.*?(Lens|Map|Input Range|Modal|Search Date|Search Box|Form Input|Datetime)).)*$/
 });
