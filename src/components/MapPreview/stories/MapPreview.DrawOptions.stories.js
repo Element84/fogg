a8014b1ebc2503148414e3696dcd694b0b6c07ea
Colby Fayock <fayock@gmail.com>
2020-09-08 16:31:05 -0400

    [JAM-1360] adding ability to pass colors to shapes and events

---
+++ src/components/MapPreview/stories/MapPreview.DrawOptions.stories.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Story from '../../../../stories/helpers/Story';
+
+import MapPreview from '../';
+
+import featureCollection from '../../../../tests/fixtures/feature-collection.json';
+
+const STORY_COMPONENT = 'Map Preview';
+const STORY_NAME = 'Feature Events';
+
+const stories = storiesOf(`Components/${STORY_COMPONENT}`, module);
+
+const features = {
+  ...featureCollection,
+  features: featureCollection.features.map((feature, index) => {
+    return {
+      ...feature,
+      properties: {
+        ...feature.properties,
+        onClick: function () {
+          console.log(`Click: ${index}`, feature);
+        },
+        onMouseover: function () {
+          console.log(`Mouse Over: ${index}`, feature);
+        },
+        onMouseout: function () {
+          console.log(`Mouse Out: ${index}`, feature);
+        }
+      }
+    };
+  })
+};
+
+stories.add(STORY_NAME, () => {
+  return (
+    <Story component={STORY_COMPONENT} name={STORY_NAME}>
+      <MapPreview geoJson={features} />
+    </Story>
+  );
+});
