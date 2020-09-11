1716c0b4d078fcd3e4570035fa0135e235aaf229
Colby Fayock <fayock@gmail.com>
2020-09-11 12:47:11 -0400

    [JAM-1359] setting an option to allow designation of aoi for mappreview

--- src/components/MapPreview/stories/MapPreview.DrawOptions.stories.js
+++ src/components/MapPreview/stories/MapPreview.DrawOptions.stories.js
@@ -13,6 +13,9 @@ const STORY_NAME = 'Draw Options';
 const stories = storiesOf(`Components/${STORY_COMPONENT}`, module);
 
 stories.add(STORY_NAME, () => {
+  // Define an AOI
+  featureCollection.features[0].properties.featureType = 'aoi';
+
   return (
     <Story component={STORY_COMPONENT} name={STORY_NAME}>
       <MapPreview geoJson={featureCollection} />
