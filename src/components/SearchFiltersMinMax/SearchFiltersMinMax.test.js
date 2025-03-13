c4cfaac7419e888d02a30cc2cfe5bdab2d22bbe7
Ryan Herman <ryan.herman@capellaspace.com>
2025-03-13 15:47:44 -0600

    Fix test naming

--- src/components/SearchFiltersMinMax/SearchFiltersMinMax.test.js
+++ src/components/SearchFiltersMinMax/SearchFiltersMinMax.test.js
@@ -6,10 +6,10 @@ import SearchFiltersMinMax from './';
 describe('SearchFiltersMinMax', () => {
   const notice = 'Values outside of limits will default to min and max.';
   it('renders a SearchFiltersMinMax', () => {
-    const Range = shallow(<SearchFiltersMinMax />);
+    const MinMax = shallow(<SearchFiltersMinMax />);
 
-    expect(Range.find('div.search-filters-minmax-input').exists()).toEqual(true);
-    expect(Range.find('span.error').exists()).toEqual(true);
-    expect(Range.find('p.search-filters-range-note').text()).toEqual(notice);
+    expect(MinMax.find('div.search-filters-minmax-input').exists()).toEqual(true);
+    expect(MinMax.find('span.error').exists()).toEqual(true);
+    expect(MinMax.find('p.search-filters-minmax-note').text()).toEqual(notice);
   });
 });
