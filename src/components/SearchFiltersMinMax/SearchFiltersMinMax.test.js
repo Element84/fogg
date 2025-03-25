e40f33ff6254218db7d5e80f2c34be9d7242e550
Ryan Herman <ryan.herman@capellaspace.com>
2025-03-25 16:02:24 -0600

    Add error display tests

--- src/components/SearchFiltersMinMax/SearchFiltersMinMax.test.js
+++ src/components/SearchFiltersMinMax/SearchFiltersMinMax.test.js
@@ -12,4 +12,42 @@ describe('SearchFiltersMinMax', () => {
     expect(MinMax.find('span.error').exists()).toEqual(true);
     expect(MinMax.find('p.search-filters-minmax-note').text()).toEqual(notice);
   });
+
+  it('displays an error when min is out of range', () => {
+    const limits = {
+      min: {
+        min: 0,
+        max: 25
+      },
+      max: {
+        min: 0,
+        max: 50
+      }
+    };
+
+    const MinMax = shallow(<SearchFiltersMinMax id="test" limits={limits} />);
+    MinMax.find('#test-minmax-min').simulate('change', { target: { id: 'test-minmax-min', value: '30' } });
+    MinMax.update();
+
+    expect(MinMax.findWhere(node => node.text() === 'Invalid Min Value').exists()).toEqual(true);
+  });
+
+  it('displays an error when max is out of range', () => {
+    const limits = {
+      min: {
+        min: 0,
+        max: 25
+      },
+      max: {
+        min: 0,
+        max: 50
+      }
+    };
+
+    const MinMax = shallow(<SearchFiltersMinMax id="test" limits={limits} />);
+    MinMax.find('#test-minmax-max').simulate('change', { target: { id: 'test-minmax-max', value: '55' } });
+    MinMax.update();
+
+    expect(MinMax.findWhere(node => node.text() === 'Invalid Max Value').exists()).toEqual(true);
+  });
 });
